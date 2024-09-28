dc41e9810aefde819d7c334d19fd76f1e24c8784
earl.wagner@innovateworld.io <earl.wagner@innovateworld.io>
2024-09-28 16:46:37 +0100

    setup react, tailwind, shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,18 @@ import { defineConfig } from "astro/config";
 
 import cloudflare from "@astrojs/cloudflare";
 
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare(),
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
 });
